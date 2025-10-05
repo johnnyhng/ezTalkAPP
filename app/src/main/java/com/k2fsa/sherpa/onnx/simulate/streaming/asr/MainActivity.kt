3f6e2ed75cf111c2bf735f12c48c9b6092f9a66b
johnnyhng <johnny.hansyun@gmail.com>
2025-10-05 18:53:40 +0800

    refactor(Android): Rename app to ezTalk
    This commit rebrands the `SimulateStreamingAsr` Android application to "ezTalk".
    
    The app name displayed to the user and the title text on the main screen have been updated to reflect this change.

--- app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/MainActivity.kt
+++ app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/MainActivity.kt
@@ -104,7 +104,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 ),
                 title = {
                     Text(
-                        "Next-gen Kaldi: Simulate real-time speech recognition",
+                        "ezTalk: real-time speech recognition",
                         fontWeight = FontWeight.Bold,
                     )
                 },
