e48722ee69f8ee5561ae23c3dab5bbe221d62e53
johnnyhng <johnny.hansyun@gmail.com>
2025-10-17 23:43:44 +0800

    feat(android): Add `canCheck` property to Transcript data class
    This commit introduces a new boolean property, `canCheck`, to the `Transcript` data class.
    
    The `canCheck` field, which defaults to `true`, provides a mechanism to control whether a transcript item can be selected or checked in the UI.

--- app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/data/classes/Transcript.kt
+++ app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/data/classes/Transcript.kt
@@ -4,5 +4,6 @@ data class Transcript(
     val recognizedText: String,
     var modifiedText: String = recognizedText,
     var wavFilePath: String,
-    var checked: Boolean = false
+    var checked: Boolean = false,
+    var canCheck: Boolean = true
 )
